adbe7bbf099ccfcdba3b032205bc6f3509954ba9
Berzeg07 <berezg86@gmail.com>
2017-06-05 16:26:05 +0400

    31.05

--- js/common.js
+++ js/common.js
@@ -3,115 +3,169 @@ $(document).ready(function(){
 	// Мобильное меню 
 	$(".fa-bars").click(function(m){
 		m.preventDefault();
-		$(".header__menu-box").slideToggle(500);
+		$(".main-menu__box3, .main-menu__box4, .main-menu__box5, .main-menu__box6").slideToggle(500);
 	});
 	// Конец Мобильное меню
 
-	// Слайдер swiper
-	var swiper = new Swiper('.swiper-container', {
-		pagination: '.swiper-pagination',
-		effect: 'coverflow',
-		grabCursor: true,
-		centeredSlides: true,
-		loop: true,
-		spaceBetween: -250,
-		slidesPerView: 'auto',
-		coverflow: {
-			rotate: 50,
-			stretch: 0,
-			depth: 100,
-			modifier: 1,
-			slideShadows : true,
+    // Табы главная
+    $('.service-box a').click(function(e) {
+        e.preventDefault();
+        $('a').removeClass('active_tab');
+        $(this).addClass('active_tab');
+        var tab = $(this).attr('href');
+        $('.detail-box').not(tab).css({'display':'none'});
+        $(tab).fadeIn(400);       
+    });
+    $('.service-box a:first').click();
+    // Конец Табы
 
-		}
-	});
 
-    // Fancybox
-    $("a[rel=group]").fancybox({
-        'transitionIn' : 'none',
-        'transitionOut' : 'none',
-        'titlePosition' : 'over',
-        'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
-            return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+      // Табы новости
+      $('.news__box_data a').click(function(i) {
+        i.preventDefault();
+        $('a').removeClass('active_tab2');
+        $(this).addClass('active_tab2');
+        var tab = $(this).attr('href');
+        $('.cart').not(tab).css({'display':'none'});
+        $(tab).fadeIn(400);       
+    });
+      $('.news__box_data a:first').click();
+    // Конец Табы
+
+        /* Slider
+        =====================*/
+        var owl2 = $("#slider__actual");
+        owl2.owlCarousel({
+            loop:true,
+            nav:true, 
+            autoplay:false,
+            smartSpeed:1000,
+            margin:25,
+        center:false,     //если нужны обрезаные края
+        navText:['<span class="nav-left"></span>','<span class="nav-right"></span>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2 
+            },
+            768:{
+                items:3
+            }, 
+             1240:{
+                items:4
+            },      
         }
     });
 
-    // Modal window
-    $('a[name=modal]').click(function(e) {
-    	e.preventDefault();
-    	var id = $(this).attr('href');
-    	var maskHeight = $(document).height();
-    	var maskWidth = $(window).width();
-    	$('#mask').css({'width':maskWidth,'height':maskHeight});
-    	$('#mask').fadeTo("slow",0.8); 
-    	var winH = $(window).height();
-    	var winW = $(window).width();
-    	posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement ||document.body.parentNode || document.body).scrollTop;
-    	$(id).css('top',  posTop+50);
-    	$(id).css('left', winW/2-$(id).width()/2);
-    	$(id).fadeIn(500); 
+    /* Slider 2
+    =====================*/
+    var owl = $("#slider__news");
+    owl.owlCarousel({
+        loop:true,
+        nav:true, 
+        autoplay:false,
+        smartSpeed:1000,
+        margin:5,
+        center:false, 
+        navText:['<span class="nav-left1"></span>','<span class="nav-right2"></span>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:1 
+            },
+            768:{
+                items:1
+            },      
+        }
     });
-    $('.window .dd-close').click(function (e) {
-    	e.preventDefault();
-    	$('#mask, .window').hide();
-    	$('.window').hide();
-    }); 
 
-    $('#mask, .an-exit__krest').click(function () {
-    	$('#mask').hide();
-    	$('.window').hide();
-    }); 
-    $(".phone").mask("+ 7 (999) 999 - 99 - 99?"); 
-    $(".form-message").submit(function() { 
-    	var tel = $(this).find('input[name="phone"]');
-    	var empty = false;
-    	if (tel.val() == ""){
-    		empty = true;
-    	}
-    	if (empty == true){
-    		tel.addClass("error-input");
-    		tel.focus();
-    	}else{
-    		var form_data = $(this).serialize(); 
-    		$.ajax({
-    			type: "POST", 
-    			url: "/message.php", 
-    			data: form_data,
-    			success: function() {
-    				cleanTnanks(this);
-    			}
-    		});
-    	}
-    	return false;
-    }); 
-
-    function cleanTnanks(form){
-    	$('input[type="text"]').removeClass("error-input");
-    	$("input[type=text], textarea").val("");
-    	$('.window').hide();
-    	$('a[href=#thanks]').trigger('click');
-
-    };
-
-    // css slider
-    function func1() {
-    	$('.current_item1').addClass('current_item2')
-    	$('.current_item .current_item1').removeClass('current_item2');
-    	$('.current_item .item-title').removeClass('hide');
-    	$('.current_item .item-someth').removeClass('hide');
-    }
     $(function() {
-    	$("#mySlider2").AnimatedSlider( { prevButton: "#btn_prev2", 
-    		nextButton: "#btn_next2",
-    		visibleItems: 5,
-    		infiniteScroll: true,
-    		willChangeCallback: function(obj, item) { $("#statusText").text("Will change to " + item); },
-    		changedCallback: function(obj, item) { $("#statusText").text("Changed to " + item); }
-    	});
-    	$('#btn_next2, #btn_prev2').click(function(){
-    		func1();
-    	});
-    	func1();
+
+        $(".js-nicescroll").niceScroll({
+            cursorcolor: "#166594",
+            background: "#BABABA",
+            cursorwidth: "5px",
+            cursorborder: "0",
+            cursorborderradius: "6px",
+            autohidemode: false,
+            cursorminheight: 50,
+            horizrailenabled: false,
+            railpadding: { top: -5, right: 0, left: 0, bottom: 5 },
+        });
+
     });
+
+    // Fancybox
+    // $("a[rel=group]").fancybox({
+    //     'transitionIn' : 'none',
+    //     'transitionOut' : 'none',
+    //     'titlePosition' : 'over',
+    //     'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
+    //         return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+    //     }
+    // });
+
+    // Modal window
+    // $('a[name=modal]').click(function(e) {
+    // 	e.preventDefault();
+    // 	var id = $(this).attr('href');
+    // 	var maskHeight = $(document).height();
+    // 	var maskWidth = $(window).width();
+    // 	$('#mask').css({'width':maskWidth,'height':maskHeight});
+    // 	$('#mask').fadeTo("slow",0.8); 
+    // 	var winH = $(window).height();
+    // 	var winW = $(window).width();
+    // 	posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement ||document.body.parentNode || document.body).scrollTop;
+    // 	$(id).css('top',  posTop+50);
+    // 	$(id).css('left', winW/2-$(id).width()/2);
+    // 	$(id).fadeIn(500); 
+    // });
+    // $('.window .dd-close').click(function (e) {
+    // 	e.preventDefault();
+    // 	$('#mask, .window').hide();
+    // 	$('.window').hide();
+    // }); 
+
+    // $('#mask, .an-exit__krest').click(function () {
+    // 	$('#mask').hide();
+    // 	$('.window').hide();
+    // }); 
+    // $(".phone").mask("+ 7 (999) 999 - 99 - 99?"); 
+    // $(".form-message").submit(function() { 
+    // 	var tel = $(this).find('input[name="phone"]');
+    // 	var empty = false;
+    // 	if (tel.val() == ""){
+    // 		empty = true;
+    // 	}
+    // 	if (empty == true){
+    // 		tel.addClass("error-input");
+    // 		tel.focus();
+    // 	}else{
+    // 		var form_data = $(this).serialize(); 
+    // 		$.ajax({
+    // 			type: "POST", 
+    // 			url: "/message.php", 
+    // 			data: form_data,
+    // 			success: function() {
+    // 				cleanTnanks(this);
+    // 			}
+    // 		});
+    // 	}
+    // 	return false;
+    // }); 
+
+    // function cleanTnanks(form){
+    // 	$('input[type="text"]').removeClass("error-input");
+    // 	$("input[type=text], textarea").val("");
+    // 	$('.window').hide();
+    // 	$('a[href=#thanks]').trigger('click');
+
+    // };
+
+
 
 });//END READY
---
+++ opros.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Главная</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/fonts.css">
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<link rel="stylesheet" href="css/animated-slider.css" />
+	<link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
+	
+</head>
+<body class="bg_opros">
+
+
+	<div class="opros_wrap">
+		<div class="container__opros">
+			<div class="opros__txt">
+				<h1>Электронный опросник автосохранение</h1>
+				<p>Просим Вас ответить буквально на несколько вопросов. Это займет меньше минуты.</p>
+			</div>
+
+			<div class="opros-radio">
+				<div class="opros_title">
+					<span>Имеется ли в вашей семье автомобиль</span><br/>
+					<span>(Выберите один ответ)</span>
+				</div>
+				<form action="#">
+					<div class="radio_box">
+						<label>
+							<input class="checkbox" type="radio" name="checkbox-test" >
+							<span class="checkbox-custom"></span>
+							<span class="label">Нет</span>
+						</label>
+					</div>
+					<div class="radio_box">
+						<label>
+							<input class="checkbox" type="radio" name="checkbox-test" checked>
+							<span class="checkbox-custom"></span>
+							<span class="label">Да</span>
+						</label>
+					</div>
+				</form>
+			</div>
+			
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<!-- JS_BLOCK -->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script src="js/responsiveTabs.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/fonts.js"></script>
+	<script src="js/common.js"></script>
+	<script src="js/jquery.nicescroll.min.js"></script>
+	<script src="js/jquery.cssslider.js"></script>
+	<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+
+</body>
+</html>
